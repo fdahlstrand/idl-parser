af5c0b66b4ef7b6b6ef2b7f3eadcebdda1346997
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-08-07 22:57:20 +0200

    Setup initial lexer structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+
+#[derive(PartialEq, Debug)]
+enum TokenType {
+    SEMICOLON,
+}
+
+#[derive(PartialEq, Debug)]
+struct Token {
+    typ: TokenType,
+    literal: String,
+}
+
+impl Token {
+    fn new(t: TokenType, literal: &str) -> Self {
+        return Token {
+            typ: t,
+            literal: literal.to_string(),
+        };
+    }
+}
+
+fn next_token(_: &str) -> Token {
+    return Token::new(TokenType::SEMICOLON, ";");
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_next_token() {
+        let input = String::from("=");
+        let token = next_token(&input);
+        assert_eq!(token, Token::new(TokenType::SEMICOLON, ";"));
+    }
 }
